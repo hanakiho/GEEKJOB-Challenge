4d11458270b40694c7eee5d59657f88f793d42b7
花木未怜 <pwwqasboo055@gmail.com>
2018-04-20 17:57:11 +0900

    メソッド11-6提出しました。

---
+++ CampChallenge_java/011.メソッド/method6.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author hanakimisato
+ */
+@WebServlet(name = "method6", urlPatterns = {"/method6"})
+public class method6 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    ArrayList<String> profile1(){
+        ArrayList<String> data1=new ArrayList<String>();
+        data1.add("1992年１０月３日");
+        data1.add("東京都");
+        data1.add("1246");
+        
+         return data1; 
+    }
+    ArrayList<String> profile2(){
+        ArrayList<String> data2=new ArrayList<String>();
+        data2.add("1993年9月３日");
+        data2.add("神奈川県");
+        data2.add("2231");
+        
+         return data2;  
+    }
+    
+    ArrayList<String> profile3(){
+        ArrayList<String> data3=new ArrayList<String>();
+        data3.add("1998年１1月18日");
+        data3.add("大阪府");
+        data3.add("1111");
+        
+         return data3;  
+    }
+    
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            
+            
+                
+                out.print(profile1());
+                out.print(profile2());   
+                out.print(profile3()); 
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
