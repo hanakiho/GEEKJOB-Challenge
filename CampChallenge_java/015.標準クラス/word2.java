c733d157a242e49c7ab79594aea8630cd6890697
花木未怜 <pwwqasboo055@gmail.com>
2018-04-29 16:59:35 +0900

    標準クラス15-6提出します。

---
+++ CampChallenge_java/015.標準クラス/word2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaパッケージ;
+
+/**
+ *
+ * @author hanakimisato
+ */
+public class word2 {
+    public static void main(String[] args) {
+        String address="zikosyoukai@gmail.com";
+        //@を文字数を画面表示する。
+        String mail=address.substring(12);
+        System.out.print(mail.length());
+    }
+}
